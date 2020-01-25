f52901080758df54d2a8f079232983b3d68f0b14
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-25 18:51:44 +0700

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./app.routes";
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { ErrorPageComponent } from './pages/error-page/error-page.component';
-import { ProductsPageComponent } from './pages/products-page/products-page.component';
-import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
+import { ErrorPageComponent } from "./pages/error-page/error-page.component";
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, AboutPageComponent, ContactPageComponent, ErrorPageComponent, ProductsPageComponent, ProductPageComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    ErrorPageComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
 import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
 import { ErrorPageComponent } from "./pages/error-page/error-page.component";
-import { ProductsPageComponent } from "./pages/products-page/products-page.component";
-import { ProductPageComponent } from "./pages/product-page/product-page.component";
 
 export const appRoutes: Routes = [
   { path: "", component: HomePageComponent },
@@ -15,14 +13,6 @@ export const appRoutes: Routes = [
   {
     path: "contact",
     component: ContactPageComponent
-  },
-  {
-    path: "products",
-    component: ProductsPageComponent
-  },
-  {
-    path: "products/:id",
-    component: ProductPageComponent
   },
   { path: "404", component: ErrorPageComponent },
   { path: "**", redirectTo: "/404" }
