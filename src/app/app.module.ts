cec3571a7b1de96af9e41c29e8680b8bc1b40c00
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-25 18:53:32 +0700

    Reduce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./app.routes";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
-import { AboutPageComponent } from "./pages/about-page/about-page.component";
-import { RouterModule } from "@angular/router";
-import { appRoutes } from "./app.routes";
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { ErrorPageComponent } from './pages/error-page/error-page.component';
-import { ProductsPageComponent } from './pages/products-page/products-page.component';
-import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ErrorPageComponent } from "./pages/error-page/error-page.component";
+import { ProductsPageComponent } from "./pages/products-page/products-page.component";
+import { ProductPageComponent } from "./pages/product-page/product-page.component";
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, AboutPageComponent, ContactPageComponent, ErrorPageComponent, ProductsPageComponent, ProductPageComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ErrorPageComponent,
+    ProductsPageComponent,
+    ProductPageComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,11 @@
 import { Routes } from "@angular/router";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
-import { AboutPageComponent } from "./pages/about-page/about-page.component";
-import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
 import { ErrorPageComponent } from "./pages/error-page/error-page.component";
 import { ProductsPageComponent } from "./pages/products-page/products-page.component";
 import { ProductPageComponent } from "./pages/product-page/product-page.component";
 
 export const appRoutes: Routes = [
   { path: "", component: HomePageComponent },
-  {
-    path: "about",
-    component: AboutPageComponent
-  },
-  {
-    path: "contact",
-    component: ContactPageComponent
-  },
   {
     path: "products",
     component: ProductsPageComponent
