556f2ed2622c8a0b7502413b50f1b431a73dfa6c
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-25 15:47:04 +0700

    Create routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
-// import { RouterModule } from "@angular/router";
-// import { appRoutes } from "./app.routes";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./app.routes";
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, AboutPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { AboutPageComponent } from "./pages/about-page/about-page.component";
+
+export const appRoutes: Routes = [
+  { path: "", component: HomePageComponent },
+  {
+    path: "about",
+    component: AboutPageComponent
+  }
+];
