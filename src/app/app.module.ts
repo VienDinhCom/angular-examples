ca49f92e97ef80dec30088a372518d8f8278958a
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-25 16:02:06 +0700

    Create contact-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./app.routes";
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, AboutPageComponent],
+  declarations: [AppComponent, HomePageComponent, AboutPageComponent, ContactPageComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from "@angular/router";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
+import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
 
 export const appRoutes: Routes = [
   { path: "", component: HomePageComponent },
   {
     path: "about",
     component: AboutPageComponent
+  },
+  {
+    path: "contact",
+    component: ContactPageComponent
   }
 ];
