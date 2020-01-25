8f4289111513fceaccc6de1bf92341cbe6290be8
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-25 15:46:46 +0700

    Create pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { AboutPageComponent } from "./pages/about-page/about-page.component";
+// import { RouterModule } from "@angular/router";
+// import { appRoutes } from "./app.routes";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomePageComponent, AboutPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
