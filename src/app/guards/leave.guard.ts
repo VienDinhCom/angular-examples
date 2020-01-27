a298e4feb5d4f03e3d8c328ccc384faa63f25a4c
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-27 14:54:57 +0700

    Micro changes

--- src/app/guards/leave.guard.ts
+++ src/app/guards/leave.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { CanDeactivate } from "@angular/router";
-import { Observable } from "rxjs";
 
 export interface CanComponentDeactivate {
   canLeave: () => string;
