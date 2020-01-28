4dd4550eb6cd5f5a82056797a7438a92f81ff593
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-28 16:58:07 +0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
+import { appRoutes } from "./app.routes";
+
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { ErrorPageComponent } from "./pages/error-page/error-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
-import { appRoutes } from "./app.routes";
-import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { RouterModule } from "@angular/router";
     ErrorPageComponent,
     AboutPageComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
