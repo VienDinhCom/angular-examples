70560c9b7bb75680ae900ff3469946230d0558d0
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-29 10:15:58 +0700

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
@@ -18,7 +17,7 @@ import { ErrorPageComponent } from "./pages/error-page/error-page.component";
     ContactPageComponent,
     ErrorPageComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
