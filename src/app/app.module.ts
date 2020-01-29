3996a8c1298c85290e0f5ed2db2966130bd9d21b
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-29 09:30:15 +0700

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { CoreModule } from "@core/core.module";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from '@core/core.module';
 
-import { AppComponent } from "./app.component";
-import { appRoutes } from "./app.routes";
-import { RouterModule } from "@angular/router";
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CoreModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
