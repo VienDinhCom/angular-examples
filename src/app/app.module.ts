6b1981b8ec4a68c73e27e49531cab076050d2fd1
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-29 13:03:01 +0700

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { appRoutes } from "./app.routes";
 
@@ -8,15 +9,17 @@ import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { ErrorPageComponent } from "./pages/error-page/error-page.component";
 import { AboutPageComponent } from "./pages/about-page/about-page.component";
+import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ErrorPageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    ContactPageComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-contact-page",
+  templateUrl: "./contact-page.component.html",
+  styleUrls: ["./contact-page.component.scss"]
+})
+export class ContactPageComponent implements OnInit {
+  @ViewChild("formRef", { static: false }) formRef: NgForm;
+
+  formDefaultValue = {
+    title: "",
+    email: "test@me.com",
+    messsage:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat harum dolorum, totam ex ducimus laudantium minus perspiciatis necessitatibus illum enim! Vel quidem tenetur, enim suscipit beatae velit sit rerum impedit!"
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  suggestTitle() {
+    this.formRef.form.patchValue({ title: "Suggested Title" });
+  }
+
+  onSubmit(formRef: NgForm) {
+    // console.log(formRef);
+    console.log(this.formRef.value);
+    this.formRef.resetForm();
+  }
+}
