acb174972928a90985d34c10bf8242abb1c530a4
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-01-30 15:27:34 +0700

    Up

--- src/app/pages/post-list-page/post-list-page.component.ts
+++ src/app/pages/post-list-page/post-list-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { PostsService } from "src/app/services/posts.service";
+import { throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { Post } from "../../models/post.model";
 
 @Component({
   selector: "app-post-list-page",
@@ -7,9 +10,26 @@ import { PostsService } from "src/app/services/posts.service";
   styleUrls: ["./post-list-page.component.scss"]
 })
 export class PostListPageComponent implements OnInit {
+  posts: Post[] = [];
+  error: Error = null;
+  loading: boolean = true;
+
   constructor(private postsService: PostsService) {}
 
-  posts$ = this.postsService.fetch();
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.postsService
+      .fetch()
+      .pipe(
+        catchError(error => {
+          this.loading = false;
+          this.error = error;
+          return throwError(error);
+        })
+      )
+      .subscribe(posts => {
+        this.loading = false;
+        this.posts = posts;
+        this.error = null;
+      });
+  }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { throwError } from "rxjs";
-import { catchError, map } from "rxjs/operators";
 import { Post } from "../models/post.model";
 
 @Injectable({
@@ -11,8 +9,6 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   fetch() {
-    return this.http
-      .get<Post[]>("http://localhost:3000/posts")
-      .pipe(catchError(error => throwError(error)));
+    return this.http.get<Post[]>("http://localhost:3000/posts");
   }
 }
