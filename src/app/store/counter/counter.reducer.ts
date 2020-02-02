0bd93bb43a66c4ebe7fd6560a662d7bb0b16c744
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-02-02 11:41:21 +0700

    Up

--- src/app/store/counter/counter.reducer.ts
+++ src/app/store/counter/counter.reducer.ts
@@ -1,13 +1,17 @@
-import { createReducer, on } from "@ngrx/store";
+import { createReducer, on, Action } from "@ngrx/store";
 import { increment, decrement, reset } from "./counter.actions";
 
 const initialState: CounterState = 0;
 
 export type CounterState = number;
 
-export const counterReducer = createReducer(
+const reducer = createReducer(
   initialState,
   on(increment, state => state + 1),
   on(decrement, state => state - 1),
   on(reset, state => initialState)
 );
+
+export function counterReducer(state: CounterState, action: Action) {
+  return reducer(state, action);
+}
