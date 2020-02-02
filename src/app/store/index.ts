e27851df7f7d7432fad02fbeed939436da008a1c
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-02-02 13:33:43 +0700

    Up

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,6 +1,19 @@
 import { counterReducer, CounterState } from "./counter/counter.reducer";
 import * as counterActions from "./counter/counter.actions";
 import { counterSelectors } from "./counter/counter.selectors";
+import { createSlide } from "./createSlide";
+
+const counter = createSlide({
+  name: "counter",
+  initialState: 0,
+  reducers: {
+    loadCounter: (state, action) => {},
+    loadCounterSuccess: () => {},
+    loadCounterFailure: () => {}
+  }
+});
+
+console.log(counter);
 
 export interface State {
   count: CounterState;
