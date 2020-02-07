d19bb5d052041c86efe56a3d416d641068b675cf
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-02-07 17:08:42 +0700

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,19 @@ import { appRoutes } from "./app.routes";
 
 import { AppComponent } from "./app.component";
 import { ErrorPageComponent } from "./pages/error-page/error-page.component";
-import { PostsPageComponent } from './pages/posts-page/posts-page.component';
+import { PostsPageComponent } from "./pages/posts-page/posts-page.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent, ErrorPageComponent, PostsPageComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: "serverApp" }),
     RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
