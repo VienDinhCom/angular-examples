7166e401c7f72f4ffbcacb3a90a86b3f4b8e3fc4
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-02-11 11:14:48 +0700

    Remove routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
-import { Routes } from "@angular/router";
-import { HomePageComponent } from "./pages/home-page/home-page.component";
-import { ErrorPageComponent } from "./pages/error-page/error-page.component";
-import { AboutPageComponent } from "./pages/about-page/about-page.component";
+import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-  { path: "", component: HomePageComponent },
-  { path: "about", component: AboutPageComponent },
-  { path: "404", component: ErrorPageComponent },
-  { path: "**", redirectTo: "/404" }
+  // { path: "", component: HomePageComponent },
+  // { path: "about", component: AboutPageComponent },
+  // { path: "404", component: ErrorPageComponent },
+  { path: '**', redirectTo: '/404' },
 ];
