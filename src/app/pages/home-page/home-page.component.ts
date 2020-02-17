a6c8953834e979330492da261d908baaf084df69
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-02-17 11:46:50 +0700

    Up

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  sound = new Howl({
+    src: ['/assets/KrLRFBmM.mp3'],
+    autoplay: false,
+    loop: false,
+    volume: 1,
+    html5: true,
+    sprite: {
+      blast: [0, 2420],
+      laser: [3340, 5880],
+      winner: [6100, 8500],
+    },
+  });
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onPlay(n: string) {
+    this.sound.pause();
+    this.sound.play(n);
   }
-
 }
